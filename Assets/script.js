7275195075bc96c14b69791d9d57a3698d2e27fa
kalaniJ94 <kalaniJ94@gmail.com>
2023-07-22 10:54:42 -0600

    first JS work

--- Assets/script.js
+++ Assets/script.js
@@ -13,14 +13,21 @@
 var wordBlank = document.querySelector(".word-blanks");
 var win = document.querySelector(".win");
 var lose = document.querySelector(".lose");
+var container = document.querySelector("container");
 
 var timerEl = document.getElementById("timer-text");
 var startButton = document.getElementById("start-button");
 
+var quest1 = document.getElementById("box1")
+var quest2 = document.getElementById("box2")
+var quest3 = document.getElementById("box3")
+var quest4 = document.getElementById("box4")
+
+
 var questionBlank = "";
 var wordBlank = "";
 var highscore = 0;
-var isWin;
+var initials = "";
 var timer;
 
 
@@ -30,19 +37,32 @@ var timer;
 
 function startGame(){
     timerCount=60;
-    startTimer()
-    
+    startTimer();  
+    firstQuestion();
 }
 function startTimer() {
     // Starts timer
     timer = setInterval(function() {
         timerCount--;
         timerEl.textContent = timerCount;
+        if(timerCount > 0){
+            startGame.disabled=true;
+        }
+        if(timerCount===0){
+            // endGame();
+            clearInterval(timer);
+        }
     }
     , 1000);
 }
-startButton.addEventListener("click", startGame);
+// document.addEventListener("click", function(){
+//     timerCount-10;
+// })
 //Questions are multiple choice(Four choices). "Wrong!" or "Correct!" will appear when a choice is made.  Next question will appear after those two seconds. 
+function firstQuestion(){
+    questionBlank
+
+}
 
 //If an incorrect question is answered, ten seconds will be subtracted from the timer. Correct answers will award points. 
 
@@ -52,3 +72,4 @@ startButton.addEventListener("click", startGame);
 
 //When the game is over, shows highscore and prompts Initials. Initials will be saved onto a highscores list, which will be on a separate page. The Highscore page will have two buttons, one to return to game and one to clear high score list. 
 
+startButton.addEventListener("click", startGame);
