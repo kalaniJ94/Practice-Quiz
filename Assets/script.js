52edbe67e8d3517fec4453fa4fe426a909474137
kalaniJ94 <kalaniJ94@gmail.com>
2023-07-27 11:30:16 -0600

    Final README/Questions

--- Assets/script.js
+++ Assets/script.js
@@ -1,384 +1,146 @@
-// GIVEN I am taking a code quiz
-// WHEN I click the start button
-// THEN a timer starts and I am presented with a question
-// WHEN I answer a question
-// THEN I am presented with another question
-// WHEN I answer a question incorrectly
-// THEN time is subtracted from the clock
-// WHEN all questions are answered or the timer reaches 0
-// THEN the game is over
-// WHEN the game is over
-// THEN I can save my initials and my score
-// --------------------------------------------------------------------------------------------------------
+// Vars organized by selector/type
 var gameEnded = false;
-var containers = document.querySelector("#container");
 
 var timerEl = document.getElementById("timer-text");
 var startButton = document.getElementById("start-button");
-
 var questionBlank = document.getElementById("question-blank");
+var intro = document.getElementById("Intro")
+
+var containers = document.querySelector("#container");
 var wordBlank = document.querySelector("#wordblank");
 
 var questBox = document.getElementsByClassName("box")
+// question array
+var questions = [
+  {
+    question: "How many championships have the Green Bay Packers won in their history?",
+    choices: ["13", "4", "0", "8"],
+    correctAnswer: "13",
+  },
+  {
+    question: "During what year was the United States Constitution ratified?",
+    choices: ["1780", "1776 ", "1788", "1778"],
+    correctAnswer: "1788",
+  }, {
+    question: "Potatoes originated from which area?",
+    choices: ["Morocco", "Russia", "Ireland", "Peru"],
+    correctAnswer: "Peru",
+  }, {
+    question: "What year was the Iphone released",
+    choices: ["2007", "2006", "2010", "2009"],
+    correctAnswer: "2007",
+  },
+  {
+    question: "What injury did Lord Nelson receive during the Napoleon Wars ",
+    choices: ["Became incontinent", "Lost his arm", "Lost his leg", "Lost his ear"],
+    correctAnswer: "Lost his arm",
+  },
+  {
+    question: "What is the name of Louise Belcher's favorite toy?",
+    choices: ["Coochie Kopi", "Coochie Koo", "Coochie Wopi", "Coochie Belcher"],
+    correctAnswer: "Coochie Kopi",
+  },
+];
 
-var quest1 = document.getElementById("box1")
-var quest2 = document.getElementById("box2")
-var quest3 = document.getElementById("box3")
-var quest4 = document.getElementById("box4")
-
+var currentQuestionIndex = 0;
 var highscore = 0;
-var initials = "";
 var timer;
-
-//So first thing's first. There will be a start button which starts a one minute timer at the top of the page. The Start button will also reveal the first question. 
-
+// function to begin game here
 function startGame() {
-    gameEnded = false;
-    timerCount = 60;
-    startTimer();
-    firstQuestion();
+  gameEnded = false;
+  timerCount = 60;
+  startTimer();
+  showQuestion();
+  intro[0].textContent="";
 }
 function startTimer() {
-    // Starts timer
-    timer = setInterval(function () {
-        timerCount--;
-        timerEl.textContent = timerCount;
-        if (timerCount > 0) {
-            startButton.disabled = true;
-        }
-        if (timerCount === 0) {
-            endgame();
-            clearInterval(timer);
-        }
+  // Starts timer
+  timer = setInterval(function () {
+    timerCount--;
+    timerEl.textContent = timerCount;
+    if (timerCount > 0) {
+      startButton.disabled = true;
     }
-        , 1000);
+    if (timerCount === 0) {
+      clearInterval(timer);
+      endgame();
+    }
+  }
+    , 1000);
 }
-// })
-//Questions are multiple choice(Four choices). "Wrong!" or "Correct!" will appear when a choice is made.  Next question will appear after those two seconds. 
-//Thinking each question will be a function. Use Div[Children], to adjust text in each block. Addeventlistener will listen for clicks, correct box will give points. Click will end function and call next question. 
-function firstQuestion() {
-    questionBlank.innerHTML = "Hello There";
-    questBox[0].innerHTML = "true";
-    questBox[1].innerHTML = "General Kenobi";
-    questBox[2].innerHTML = "General Kenobi";
-    questBox[3].innerHTML = "General Kenobi";
+// question logic
+function showQuestion() {
+  if (currentQuestionIndex >= questions.length) {
+    clearInterval(timer);
+    endgame();
+  }
 
-    for (var i = 0; i < questBox.length; i++) {
-        questBox[i].addEventListener("click", function (event) {
+  var currentQuestion = questions[currentQuestionIndex];
+  questionBlank.innerHTML = currentQuestion.question;
 
-            var element = event.target;
-
-            if (element.matches(".box")) {
-
-                if (element.textContent === "true") {
-                    highscore += 10;
-                    wordBlank.innerHTML = "Correct!";
-                    secondQuestion();
-                }
-            } else {
-                wordBlank.innerHTML = "Wrong!";
-                timerCount -= 10;
-                setTimeout(function () {
-                    secondQuestion();
-                }, 1000);
-            }
-        });
-    }
-}
-function secondQuestion() {
-    questionBlank.innerHTML = "Hello There";
-    questBox[0].innerHTML = "true2";
-    questBox[1].innerHTML = "General Kenobi";
-    questBox[2].innerHTML = "General Kenobi";
-    questBox[3].innerHTML = "General Kenobi";
-    for (var i = 0; i < questBox.length; i++) {
-        questBox[i].addEventListener("click", function (event) {
-            event.stopPropagation();
-            var element = event.target;
-
-            if (element.matches(".box")) {
-
-                if (element.textContent === "true2") {
-                    highscore += 10;
-                    wordBlank.innerHTML = "Correct!";
-                    thirdQuestion();
-                }
-            } else if (element.textContent !== "true2") {
-                wordBlank.innerHTML = "Wrong!";
-                timerCount -= 10;
-                thirdQuestion();
-            }
-        });
-    }
+  for (var i = 0; i < questBox.length; i++) {
+    questBox[i].innerHTML = currentQuestion.choices[i];
+    questBox[i].addEventListener("click", checkAnswer);
+  }
 }
 
+function checkAnswer(event) {
+  var element = event.target;
+  var currentQuestion = questions[currentQuestionIndex];
 
-function thirdQuestion() {
-    questionBlank.innerHTML = "Hello There";
-    questBox[0].innerHTML = "true3";
-    questBox[1].innerHTML = "General Kenobi";
-    questBox[2].innerHTML = "General Kenobi";
-    questBox[3].innerHTML = "General Kenobi";
-    for (var i = 0; i < questBox.length; i++) {
-        questBox[i].addEventListener("click", function (event) {
-            event.stopPropagation();
-            var element = event.target;
-
-            if (element.matches(".box")) {
-
-                if (element.textContent === "true3") {
-                    highscore += 10;
-                    wordBlank.innerHTML = "Correct!";
-                    fourthQuestion();
-                }
-            } else if (element.textContent !== "true3") {
-                wordBlank.innerHTML = "Wrong!";
-                timerCount -= 10;
-                fourthQuestion();
-            }
-        });
-    }
-}
-
-
-function fourthQuestion() {
-    questionBlank.innerHTML = "Hello There";
-    questBox[0].innerHTML = "true4";
-    questBox[1].innerHTML = "General Kenobi";
-    questBox[2].innerHTML = "General Kenobi";
-    questBox[3].innerHTML = "General Kenobi";
-    for (var i = 0; i < questBox.length; i++) {
-        questBox[i].addEventListener("click", function (event) {
-            event.stopPropagation();
-            var element = event.target;
-
-            if (element.matches(".box")) {
-
-                if (element.textContent === "true4") {
-                    highscore += 10;
-                    wordBlank.innerHTML = "Correct!";
-                    fifthQuestion();
-                }
-            } else if (element.textContent !== "true4") {
-                wordBlank.innerHTML = "Wrong!";
-                timerCount -= 10;
-                fifthQuestion();
-            }
-        });
+  if (element.matches(".box")) {
+    if (element.textContent === currentQuestion.correctAnswer) {
+      highscore += 10;
+      wordBlank.innerHTML = "Correct!";
+    } else {
+      wordBlank.innerHTML = "Wrong!";
+      timerCount -= 10;
     }
 
-
-}
-function fifthQuestion() {
-    questionBlank.innerHTML = "Hello There";
-    questBox[0].innerHTML = "true5";
-    questBox[1].innerHTML = "General Kenobi";
-    questBox[2].innerHTML = "General Kenobi";
-    questBox[3].innerHTML = "General Kenobi";
-    for (var i = 0; i < questBox.length; i++) {
-        questBox[i].addEventListener("click", function (event) {
-            event.stopPropagation();
-            var element = event.target;
-
-            if (element.matches(".box")) {
-
-                if (element.textContent === "true5") {
-                    highscore += 10;
-                    wordBlank.innerHTML = "Correct!";
-                    sixthQuestion();
-                }
-            } else if (element.textContent !== "true5") {
-                wordBlank.innerHTML = "Wrong!";
-                timerCount -= 10;
-                sixthQuestion();
-            }
-        });
-    }
-}
-
-
-function sixthQuestion() {
-    questionBlank.innerHTML = "Hello There";
-    questBox[0].innerHTML = "true6";
-    questBox[1].innerHTML = "General Kenobi";
-    questBox[2].innerHTML = "General Kenobi";
-    questBox[3].innerHTML = "General Kenobi";
-    for (var i = 0; i < questBox.length; i++) {
-        questBox[i].addEventListener("click", function (event) {
-            event.stopPropagation();
-            var element = event.target;
-
-            if (element.matches(".box")) {
-
-                if (element.textContent === "true6") {
-                    highscore += 10;
-                    wordBlank.innerHTML = "Correct!";
-                    seventhQuestion();
-                }
-            } else if (element.textContent !== "true6") {
-                wordBlank.innerHTML = "Wrong!";
-                timerCount -= 10;
-                seventhQuestion();
-            }
-        });
-    }
-
-
-}
-function seventhQuestion() {
-    questionBlank.innerHTML = "Hello There";
-    questBox[0].innerHTML = "true7";
-    questBox[1].innerHTML = "General Kenobi";
-    questBox[2].innerHTML = "General Kenobi";
-    questBox[3].innerHTML = "General Kenobi";
-    for (var i = 0; i < questBox.length; i++) {
-        questBox[i].addEventListener("click", function (event) {
-            event.stopPropagation();
-            var element = event.target;
-
-            if (element.matches(".box")) {
-
-                if (element.textContent === "true7") {
-                    highscore += 10;
-                    wordBlank.innerHTML = "Correct!";
-                    eighthQuestion();
-                }
-            } else if (element.textContent !== "true7") {
-                wordBlank.innerHTML = "Wrong!";
-                timerCount -= 10;
-                eighthQuestion();
-            }
-        });
-    }
-
-
-}
-function eighthQuestion() {
-    questionBlank.innerHTML = "Hello There";
-    questBox[0].innerHTML = "true8";
-    questBox[1].innerHTML = "General Kenobi";
-    questBox[2].innerHTML = "General Kenobi";
-    questBox[3].innerHTML = "General Kenobi";
-    for (var i = 0; i < questBox.length; i++) {
-        questBox[i].addEventListener("click", function (event) {
-            event.stopPropagation();
-            var element = event.target;
-
-            if (element.matches(".box")) {
-
-                if (element.textContent === "true8") {
-                    highscore += 10;
-                    wordBlank.innerHTML = "Correct!";
-                    ninthQuestion();
-                }
-            } else if (element.textContent !== "true8") {
-                wordBlank.innerHTML = "Wrong!";
-                timerCount -= 10;
-                ninthQuestion();
-            }
-        });
-    }
-}
-
-function ninthQuestion() {
-    questionBlank.innerHTML = "Hello There";
-    questBox[0].innerHTML = "true9";
-    questBox[1].innerHTML = "General Kenobi";
-    questBox[2].innerHTML = "General Kenobi";
-    questBox[3].innerHTML = "General Kenobi";
-    for (var i = 0; i < questBox.length; i++) {
-        questBox[i].addEventListener("click", function (event) {
-            event.stopPropagation();
-            var element = event.target;
-
-            if (element.matches(".box")) {
-
-                if (element.textContent === "true9") {
-                    highscore += 10;
-                    wordBlank.innerHTML = "Correct!";
-                    tenthQuestion();
-                }
-            } else if (element.textContent !== "true9") {
-                wordBlank.innerHTML = "Wrong!";
-                timerCount -= 10;
-                tenthQuestion();
-            }
-        });
-    }
-
-}
-function tenthQuestion() {
-    questionBlank.innerHTML = "Hello There";
-    questBox[0].innerHTML = "true0";
-    questBox[1].innerHTML = "General Kenobi";
-    questBox[2].innerHTML = "General Kenobi";
-    questBox[3].innerHTML = "General Kenobi";
-    for (var i = 0; i < questBox.length; i++) {
-        questBox[i].addEventListener("click", function (event) {
-            event.stopPropagation();
-            var element = event.target;
-
-            if (element.matches(".box")) {
-
-                if (element.textContent === "true0") {
-                    highscore += 10;
-                    wordBlank.innerHTML = "Correct!";
-                    clearInterval(timer);
-                    endgame();
-
-                }
-            } else if (element.textContent !== "true0") {
-                wordBlank.innerHTML = "Wrong!";
-                timerCount -= 10;
-                clearInterval(timer);
-                endgame();
-            }
-        });
-    }
-}
-
-
-//If an incorrect question is answered, ten seconds will be subtracted from the timer. Correct answers will award points. 
-
-//DO NOT WORRY ABOUT QUESTION CONTENT YET! THAT WILL BE EASY TO FIX AT THE END!
-
-// IF timer === 0, end game. OR if all questions are done, the game ends. 
-
-function endgame() {
-    if (!gameEnded) {
-        gameEnded = true;
-    }
-   
-    var initials = window.prompt("Your score is: " + highscore + "! Enter your initials to view your place on the high score list!")
-        if(initials.length > 2){
-            window.alert("Please enter initials only");
-            endgame();
-            return;
-    }
-    {
-        if (!initials){
-          repeat;
-        }
+    currentQuestionIndex++;
+    setTimeout(function () {
+      wordBlank.innerHTML = "";
+      for (var i = 0; i < questBox.length; i++) {
+        questBox[i].removeEventListener("click", checkAnswer);
       }
-};
-// Save the high scores and initials in local storage
-var highScores = JSON.parse(localStorage.getItem("highScores")) || [];
-highScores.push({ initials: initials, score: highscore });
-localStorage.setItem("highScores", JSON.stringify(highScores));
-
-var repeat = window.confirm("Play again?")
-if (repeat) {
-    gameEnded = false;
-    highscore = 0;
-    timerCount = 60;
-    startGame();
+      showQuestion();
+    }, 1000);
+  }
 }
-            
+// endgame function here
+function endgame() {
+  if (!gameEnded) {
+    gameEnded = true;
+  }
 
+  var initials = window.prompt(
+    "Your score is: " + highscore + "! Enter your initials to view your place on the high score list!"
+  );
+  {
+    if (!initials) {
+      return;
+    }
+  }
+  if (initials.length > 2) {
+    window.alert("Please enter initials only");
+    endgame();
+    return;
+  }
+  // logic for highscore sheet
+  var highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+  highScores.push({ initials: initials, score: highscore });
+  localStorage.setItem("highScores", JSON.stringify(highScores));
 
-
-
-
-//When the game is over, shows highscore and prompts Initials. Initials will be saved onto a highscores list, which will be on a separate page. The Highscore page will have two buttons, one to return to game and one to clear high score list. 
+  var playAgain = window.confirm("Play again?");
+  if (playAgain) {
+    gameEnded = false;
+    currentQuestionIndex = 0;
+    highscore = 0;
+    startGame();
+  }
+  else{
+    window.alert("Thanks for playing!");
+  }
+}
 
 startButton.addEventListener("click", startGame);
--- README.md
+++ README.md
@@ -1,34 +1,16 @@
 # Practice-Quiz
-# <Your-Project-Title>
 
 ## Description
 
-Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
+This project was done in order to flex, and develop, new Javascript skills. It was an interesting journey, from conception, to psuedocoding, to deployment. 
 
-- What was your motivation?
-- Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
-- What problem does it solve?
-- What did you learn?
+My first, original, vision for the project was a set of functions that used innerHTML to affect and manipulate the text displaying in the <li>, using an event listener to listen for a click on the correct box, and then calling the next question function. I got very close using this method, however the problem I kept running into was that the "wrong" box was never clickable. I had to rewrite this code as a looping function of arrarys, and I am very happy with the final project. 
 
-## Table of Contents (Optional)
-
-If your README is long, add a table of contents to make it easy for users to find what they need.
-
-- [Installation](#installation)
-- [Usage](#usage)
-- [Credits](#credits)
-- [License](#license)
-
-## Installation
-
-What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+I learned so many important lessons regarding JS during this lesson, doubtless that I will utilize for the rest of my career. 
 
 ## Usage
 
-Provide instructions and examples for use. Include screenshots as needed.
-
-To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative file path, add it to your README using the following syntax:
-
+Accessing the webpage requires only an internet connection and an internet browser. I recommend Google Chrome, for it's versatility.
 ![alt text](assets/images/screenshot.png)
 
 ## Credits
@@ -45,26 +27,34 @@ https://stackoverflow.com/questions/55377064/javascript-localstorage-only-one-va
 
 ## License
 
-The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+MIT License
+
+Copyright (c) 2023 kalaniJ94
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 
 ---
 
 🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
 
-## Badges
-
-![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
-
-Badges aren't necessary, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
-
-## Features
-
-If your project has a lot of features, list them here.
 
 ## How to Contribute
 
-If you created an application or package and would like other developers to contribute to it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
-
-## Tests
-
-Go the extra mile and write tests for your application. Then provide examples on how to run them here
+How to Contribute
+If you see anything that can be improved upon in this code, please feel free to reach out! I am new to this environment and as they say, collaboration is key!
